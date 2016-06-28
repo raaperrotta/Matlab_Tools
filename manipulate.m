afdbd5f47abc7dce35c550831124f1c261f3b134
raaperrotta <raaperrotta@gmail.com>
2016-06-28 13:51:01 -0400

    Added symlog and current manipulate from NAPIE91

--- manipulate.m
+++ manipulate.m
@@ -13,38 +13,38 @@ function varargout = manipulate(varargin)
 %
 %   MANIPULATE(...,'UpdateMode',MODE) allows the user to set the frequency
 %   with which the function is called.
-%     'Low'    Refresh only when slider is released (after drag), when
-%              arrow buttons are clicked, or when edit field is altered.
+%     'Low'    Refresh only when slider is released (after drag) or
+%              clicked, or when edit field is altered.
 %     'High'   Also refresh continuously as slider is dragged. [Default]
 %     'Manual' Refresh only when user presses button.
 %
 %   MANIPULATE(...,'SliderStep',[SMALL BIG]) allows the user to set the
 %   step size of incremental slider increases and decreases. See UICONTROL
 %   for details.
-
+% 
 % Examples:
-%   >> x = 1:.001:10;
-%   >> gca, L = plot(1,1,1,1);
-%   >> myfunc = @(i,f,theta)set(L(i),'XData',x,'YData',sin(10*x)+sin(f*x+theta));
-%   >> h=manipulate(@(f,theta)myfunc(1,f,theta),{'frequency1',10,20},{'phase1',0,2*pi});
-%   >> manipulate(h,@(f,theta)myfunc(2,f,theta),{'frequency2',10,20},{'phase2',0,2*pi})
+%   x = 1:.001:10;
+%   gca, L = plot(1,1,1,1);
+%   myfunc = @(i,f,theta)set(L(i),'XData',x,'YData',sin(10*x)+sin(f*x+theta));
+%   h=manipulate(@(f,theta)myfunc(1,f,theta),{'frequency1',10,20},{'phase1',0,2*pi});
+%   manipulate(h,@(f,theta)myfunc(2,f,theta),{'frequency2',10,20},{'phase2',0,2*pi})
 %
 %   Plots the interference of two sine waves and allows the user to
 %   manipulate the frequency and phase offset of the second wave. Then does
 %   the same again, adding the slider controls to the existing slider
 %   figure.
 %
-%   >> x = linspace(-10,10,1e4);
-%   >> fig = figure('Visible','off');
-%   >> ax = [subplot(2,1,1),subplot(2,1,2)];
-%   >> f1 = @(u,s) plot(ax(1),x,exp(-1/2*((x-u)/s).^2)/sqrt(2*pi)/s,'r');
-%   >> f2 = @(u,s) title(ax(1),sprintf('N(%.2f,%.2f)',u,s^2));
-%   >> f3 = @(u,s) plot(ax(2),x,erf((x-u)/s/sqrt(2))/2+1/2,'r');
-%   >> myfunc = @(u,s) {f1(u,s),f2(u,s),f3(u,s)};
-%   >> manipulate(fig,myfunc,{'Mean',-5,5,0},{'Standard Deviation',.01,5,1})
-%   >> linkaxes(ax,'x'), set(ax,'NextPlot','replacechildren')
-%   >> movegui(fig,'onscreen')
-%   >> set(fig,'Visible','on')
+%   x = linspace(-10,10,1e4);
+%   fig = figure('Visible','off');
+%   ax = [subplot(2,1,1),subplot(2,1,2)];
+%   f1 = @(u,s) plot(ax(1),x,exp(-1/2*((x-u)/s).^2)/sqrt(2*pi)/s,'r');
+%   f2 = @(u,s) title(ax(1),sprintf('N(%.2f,%.2f)',u,s^2));
+%   f3 = @(u,s) plot(ax(2),x,erf((x-u)/s/sqrt(2))/2+1/2,'r');
+%   myfunc = @(u,s) {f1(u,s),f2(u,s),f3(u,s)};
+%   manipulate(fig,myfunc,{'Mean',-5,5,0},{'Standard Deviation',.01,5,1})
+%   linkaxes(ax,'x'), set(ax,'NextPlot','replacechildren')
+%   movegui(fig,'onscreen')
+%   set(fig,'Visible','on')
 %
 %   Plots the normal distribution and its cumuliative distribution on
 %   separate subplots and allows the user to manipulate the mean and
@@ -54,8 +54,6 @@ function varargout = manipulate(varargin)
 %
 % Created by:
 %   Robert Perrotta
-% Last Revised:
-%   2013-11-25
 
 % Possible revisions:
 %   Don't update discreet sliders unless value changes.
